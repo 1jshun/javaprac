df65b62a8e5eb5741796d1cc2476e08f70e48f29
NahunE <myjsh6547@naver.com>
2026-01-28 11:21:29 +0900

    OCP 적용

--- src/main/java/com/be24/api/common/AppConfig.java
+++ src/main/java/com/be24/api/common/AppConfig.java
@@ -1,8 +1,6 @@
 package com.be24.api.common;
 
-import com.be24.api.Board.BoardController;
-import com.be24.api.Board.BoardRepository;
-import com.be24.api.Board.BoardService;
+import com.be24.api.Board.*;
 import com.be24.api.user.UserController;
 
 import java.util.HashMap;
@@ -10,14 +8,17 @@ import java.util.Map;
 
 public class AppConfig {
     private final Map<String, Controller> controllerMap = new HashMap<>();
-    private final BoardRepository boardRepository = new BoardRepositoryImpl();
+    private final BoardRepository boardRepository = new BoardCpRepositoryImpl();
     private final BoardService boardService = new BoardService(boardRepository);
+    private final BoardController boardController = new BoardController(boardService);
 
-
+    private final UserController userController = new UserController();
     // 처음 객체가 생성될 때 controllerMap에 uri를 키로 컨트롤러 객체를 값으로 저장
     public AppConfig() {
-        controllerMap.put("/board/register", new BoardController(boardService));
-        controllerMap.put("/user/signup", new UserController());
+        controllerMap.put("/board/register", boardController);
+        controllerMap.put("/board/read", boardController);
+        controllerMap.put("/user/signup", userController);
+        controllerMap.put("/user/login", userController);
     }
 
     // 특정 uri를 이용해서 특정 컨트롤러 객체를 반환하는 메소드
